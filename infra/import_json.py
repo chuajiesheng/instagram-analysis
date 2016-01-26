ddee1290b641628fa1d1f90a087c1ef010406aa1
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-01-26 17:19:59 +0800

    Use arguments for import json

--- infra/import_json.py
+++ infra/import_json.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch
 import sys
 import json
+import getopt
 
 es = None
 
@@ -30,45 +31,95 @@ def search(index_name, query):
     for hit in res['hits']['hits']:
         print("[%(timestamp)s] %(place)s - %(temperature)s by %(device)s" % hit["_source"])
 
+
+def print_help():
+    # print help
+    print 'import_json.py -f <input_file>'
+    print ''
+    print '-h', 'Print this help file'
+    print '-n', 'Do not use index in index call'
+    print '-d doc_type', 'Print this help file'
+    print '-f file', 'Import this file'
+    print '-o', 'Start reading file at this line'
+    print '-s', 'ID for index call start with this value'
+    sys.exit()
+
+
 if __name__ == '__main__':
     address = 'http://localhost:9200'
     index_name = 'instagram'
     doc_type = ''
 
+    verbose = False
     file = ''
+    with_id = True
+    file_offset = 0
+    id_offset = 0
 
-    if len(sys.argv) > 2:
-        doc_type = sys.argv[1]
-        print 'Importing into', address + '/' + index_name + '/' + doc_type
+    try:
+        argv = sys.argv[1:]
+        opts, args = getopt.getopt(argv, 'hvnd:f:o:s:', ['help', 'verbose', 'no-index', 'doc=', 'file=', 'offset=', 'start='])
+    except getopt.GetoptError:
+        print 'import_json.py -f <input_file>'
+        sys.exit(2)
 
-        file = sys.argv[2]
-        print 'Reading file', file
-    else:
-        print 'No import file found, exiting.'
+    for opt, arg in opts:
+        if opt == '-h':
+            print_help()
+        elif opt in ('-v', '--verbose'):
+            print 'Verbose mode'
+            verbose = True
+        elif opt in ('-n', '--no-index'):
+            print 'Do not use id in the index call'
+            with_id = False
+        elif opt in ('-d', '--doc'):
+            doc_type = arg
+            print 'Document', doc_type
+        elif opt in ('-f', '--file'):
+            file = arg
+            print 'Reading file', file
+        elif opt in ('-o', '--offset'):
+            file_offset = int(arg)
+            print 'Reading file starting from line', file_offset
+        elif opt in ('-s', '--start'):
+            id_offset = int(arg)
+            print 'Index starting from id', id_offset
+
+    if doc_type is None or len(doc_type) == 0:
+        print 'Document type is a required parameter.'
         exit(1)
 
-    with_id = 1
-    if len(sys.argv) > 3:
-        with_id = int(sys.argv[3])
+    if file is None or len(file) == 0:
+        print 'File is a required parameter.'
+        exit(2)
 
     es = Elasticsearch([address])
     fp = open(file)
-    lines = 0
+    lines_added = 0
     for i, line in enumerate(fp):
-        with_id = with_id == 1
+        if i < file_offset:
+            continue
+
+        id = None
+        if with_id:
+            id = id_offset + i
+
+        if verbose:
+            print 'Putting %s/%s/%s\n%s' % (index_name, doc_type, id, line)
+
         try:
             parsed_json = json.loads(line)
         except ValueError:
-            print 'Load', id, 'failed'
+            print 'Load', i, 'failed'
 
-        if with_id:
-            put(index_name, doc_type, i, line)
-        else:
-            put(index_name, doc_type, None, line)
+        try:
+            put(index_name, doc_type, id, line)
+        except Exception:
+            print 'Retry %s/%s/%s\n%s' % (index_name, doc_type, id, line)
 
-        lines += 0
+        lines_added += 0
 
     fp.close()
-    print lines, 'added.'
+    print lines_added, 'added.'
 
 
