e4ef306ff6b95735e4f66ff27c4e40c9f1af2816
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-03-04 16:09:43 +0800

    Limit to 2 level only

--- influence/network_tree.py
+++ influence/network_tree.py
@@ -8,7 +8,7 @@ nodes = dict()
 
 def add_child(graph, source_node, source_node_level):
     print source_node, 'at level', source_node_level
-    if source_node_level > 3:
+    if source_node_level > 2:
         return
 
     r = rs.RelationshipHelper.get_relationship(source_node)
