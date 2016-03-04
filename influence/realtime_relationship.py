92b47fd24d8e8528fd6c087c0b6a2c8943f7bd50
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-03-04 20:08:14 +0800

    Add crawler for relationship, realtime

---
+++ influence/realtime_relationship.py
@@ -0,0 +1,42 @@
+import json
+import urllib
+
+ACCESS_TOKEN = '198845160.b59fbe4.d5c35ad8c01543fcac377219fffb805e'
+URL = 'https://api.instagram.com/v1/users/{}/followed-by?access_token={}'
+
+
+class RealtimeRelationshipHelper:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def download_all(insta_user_id, log=False):
+        followers = []
+        next_url = URL.format(insta_user_id, ACCESS_TOKEN)
+
+        while next_url is not None:
+            next_url, followed_by = RealtimeRelationshipHelper.download(next_url)
+            followers.extend(followed_by)
+
+            if log:
+                print 'next_url', next_url
+                print 'followed_by', len(followed_by)
+
+        if log:
+            print 'total', len(followers)
+
+        return followers
+
+    @staticmethod
+    def download(url):
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+
+        next_url = data['pagination']['next_url']
+        followed_by = [user['id'] for user in data['data']]
+        return next_url, followed_by
+
+
+if __name__ == '__main__':
+    INSTA_USER_ID = '4641204'
+    RealtimeRelationshipHelper.download_all(INSTA_USER_ID, True)
